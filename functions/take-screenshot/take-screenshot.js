de3a591e1c5a4fcc03ad8423f753c86475f6e47f
Saleh Alzahrani <saleh_ahmed@outlook.com>
2021-06-03 21:09:41 +0300

    Update take-screenshot.js

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
     
     const page = await browser.newPage();
 
-    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+    await page.goto(pageToScreenshot, { waitUntil: 'networkidle1' });
 
     const screenshot = await page.screenshot({ encoding: 'base64' });
 
